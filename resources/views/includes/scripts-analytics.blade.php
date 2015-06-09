b94cb2bf585579a12c7c046846dcd7ae8ce302bd
Jeff Mills <jeffrey.mills@outlook.com>
2015-06-09 14:24:34 -0500

    abstract some redundant code

---
+++ resources/views/includes/scripts-analytics.blade.php
@@ -0,0 +1,27 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+<script>
+    window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.3.min.js"><\/script>')
+</script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+<script src="/js/plugins.js"></script>
+
+<script src="/js/main.js"></script>
+
+<script>
+    (function(b,o,i,l,e,r){
+        b.GoogleAnalyticsObject = l;
+        b[l] || (b[l] = function(){
+            (b[l].q = b[l].q || []).push(arguments)
+        });
+        b[l].l =+ new Date;
+        e = o.createElement(i);
+        r = o.getElementsByTagName(i)[0];
+        e.src = 'https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)
+    }
+    (window,document,'script','ga'));
+    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+</script>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -43,54 +43,14 @@
         <![endif]-->
 
         <!-- Add your site or application content here -->
+
+        @include('includes.alert')
         @include('includes.header')
-
-        @if(Session::has('success'))
-        <div class="alert alert-success">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h3>Success!</h3>
-                        {{ Session::get('success') }}
-                    </div>
-                </div>
-            </div>
-        </div>
-        @endif
-
-        @if(Session::has('error'))
-        <div class="alert alert-warning">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h3>Oops!</h3>
-                        {{ Session::get('error') }}
-                    </div>
-                </div>
-            </div>
-        </div>
-        @endif
-
-
         @yield('content')
-
-
         @include('includes.footer')
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-        <script src="/js/plugins.js"></script>
-        <script src="/js/main.js"></script>
-
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='https://www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
+        @include('includes.scripts-analytics')
+        
     </body>
 </html>
