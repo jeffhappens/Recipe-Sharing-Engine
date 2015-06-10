e085df3d72715862a905f8223295fa40766e0458
Jeff Mills <jeffrey.mills@outlook.com>
2015-06-10 17:21:20 -0500

    typo

--- resources/views/includes/scripts-analytics.blade.php
+++ resources/views/includes/scripts-analytics.blade.php
@@ -1,6 +1,6 @@
 {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> --}}
 
-<script src="/js/vendor/jquery-1.11.3.min.js"></script>
+<script src="/js/vendor/jquery-1.11.2.min.js"></script>
 
 <!--
 <script>
