60f8baa8909696e7d14e95e375a60e2f91701f4f
Jeff Mills <jeffrey.mills@outlook.com>
2015-06-09 20:18:51 -0500

    PHP ENV vars

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,3 +1,3 @@
 <p>Hey {{ $first_name }}!</p>
-<p>{{ $inviter }} has invited you to share recipes at http://newrecipe.app</p>
-<p>Just visit this link to get started: http://newrecipe.app/refer/{{ $email }}/{{ $invite_token }}</p>
+<p>{{ $inviter }} has invited you to share recipes at {{ $_SERVER['HTTP_HOST'] }}</p>
+<p>Just visit this link to get started: {{ $_SERVER['HTTP_HOST'] }}/refer/{{ $email }}/{{ $invite_token }}</p>
