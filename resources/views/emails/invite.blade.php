22defa03da9b9f151792a5181da45fcf838bc918
Jeff Mills <jeffrey.mills@outlook.com>
2015-06-09 20:37:28 -0500

    email template

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,3 +1,8 @@
 <p>Hey {{ $first_name }}!</p>
 <p>{{ $inviter }} has invited you to share recipes at {{ $_SERVER['HTTP_HOST'] }}</p>
-<p>Just visit this link to get started: {{ $_SERVER['HTTP_HOST'] }}/refer/{{ $email }}/{{ $invite_token }}</p>
+<p>
+	Just visit this link to get started:
+	<a href="http://{{ $_SERVER['HTTP_HOST'] }}/refer/{{ $email }}/{{ $invite_token }}">
+		http://{{ $_SERVER['HTTP_HOST'] }}/refer/{{ $email }}/{{ $invite_token }}
+	</a>
+</p>
