d1e94eeb797eb0691579b7a1c30baab68b372816
Jeff Mills <jeffrey.mills@outlook.com>
2015-06-10 20:58:54 -0500

    Site title

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="_token" content="{{ csrf_token() }}" />
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Recipe Sharing App</title>
+        <title>Post and Share Recipes at Recipe Sharing App</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
