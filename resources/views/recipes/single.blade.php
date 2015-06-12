ab702621f2d19f92c7e4a9cbea3b9145eeafc501
Jeff Mills <jeffrey.mills@outlook.com>
2015-06-12 11:17:28 -0500

    remove trailing comma

--- resources/views/recipes/single.blade.php
+++ resources/views/recipes/single.blade.php
@@ -53,7 +53,7 @@
 				<div class="row">
 					<div class="col-md-12">
 						@if($sin->recipe_enable_comments)
-							@include('includes.comments');
+							@include('includes.comments')
 						@endif
 					</div>
 				</div>
