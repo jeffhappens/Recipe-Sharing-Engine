f4b1b2a009513ae18f617b67f8f90156ba0e6d18
Jeff Mills <jeffrey.mills@outlook.com>
2015-06-12 14:11:24 -0500

    Code cleanup

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -4,6 +4,8 @@
 	use App\Http\Controllers\Controller;
 	use Illuminate\Http\Request;
 
+
+
 	class RecipeController extends Controller {
 
 		public function recipes() {
@@ -37,13 +39,14 @@
 
 		public function single($id,$slug) {
 
-			// get recipes
-			// left join media
-			// join users
 			$data = [
 				'single' => \App\Recipe::leftJoin('users', function($leftJoin) {
 					$leftJoin->on('users.id','=','recipes.recipe_author');
-				})->join('media','media.media_recipeid','=','recipes.id')
+				})
+				->leftJoin('favorites', function($leftJoin) {
+					$leftJoin->on('favorites.favorites_recipeid','=','recipes.id');
+				})
+				->join('media','media.media_recipeid','=','recipes.id')
 				->join('instructions','instructions.instructions_recipeid','=','recipes.id')
 				->join('ingredients','ingredients.ingredient_recipeid','=','recipes.id')
 				->where('recipes.id', $id)
@@ -55,19 +58,13 @@
 					'users.display_name',
 					'media.media_filename',
 					'ingredients.ingredient_name',
-					'instructions.instructions_name'
+					'instructions.instructions_name',
+					'favorites.favorites_userid'
 
 				])
 			];
-
-/*			$data = [
-				'single' => \App\Recipe::join('users','users.id','=','recipes.recipe_author')
-				->where('recipes.id', $id)->get(),
-				'media' => \App\Media::where('media_recipeid', $id)->get(),
-				'ingredients' => \App\Ingredient::where('ingredient_recipeid', $id)->get(),				
-				'instructions' => \App\Instruction::where('instructions_recipeid', $id)->get()
-			];
-*/			return view('recipes.single', $data);
+			return view('recipes.single', $data);
+			
 		}
 
 	}
--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -1,35 +1,35 @@
-<?php namespace App\Http\Controllers;
+<?php
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
+	namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+	use App\Http\Requests;
+	use App\Http\Controllers\Controller;
+	use Illuminate\Http\Request;
 
-class SiteController extends Controller {
+	class SiteController extends Controller {
 
-	public function index() {
-		$data = [
-			'recipes' => \App\Recipe::join('users','users.id','=','recipes.recipe_author')
-			->leftJoin('media', function($leftJoin) {
-				$leftJoin->on('media.media_recipeid','=','recipes.id');
-			})
-			->leftJoin('favorites', function($leftJoin) {
-				$leftJoin->on('favorites.favorites_recipeid','=','recipes.id');
-			})
-			->get([
-				'recipes.created_at',
-				'recipes.id',
-				'recipes.recipe_title',
-				'recipes.recipe_slug',
-				'recipes.recipe_description',
-				'media.media_filename',
-				'users.username',
-				'users.display_name',
-				'favorites.favorites_userid'
-			])
-		];
-		//return $data;
-		return view('site.index', $data);
+		public function index() {
+			$data = [
+				'recipes' => \App\Recipe::join('users','users.id','=','recipes.recipe_author')
+				->leftJoin('media', function($leftJoin) {
+					$leftJoin->on('media.media_recipeid','=','recipes.id');
+				})
+				->leftJoin('favorites', function($leftJoin) {
+					$leftJoin->on('favorites.favorites_recipeid','=','recipes.id');
+				})
+				->orderby('recipes.created_at','desc')
+				->get([
+					'recipes.created_at',
+					'recipes.id',
+					'recipes.recipe_title',
+					'recipes.recipe_slug',
+					'recipes.recipe_description',
+					'media.media_filename',
+					'users.username',
+					'users.display_name',
+					'favorites.favorites_userid'
+				])
+			];
+			return view('site.index', $data);
+		}
 	}
-
-}
